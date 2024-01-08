a74b8c09468b5ceb5944eeaae07b685a0b6716a8
Niansuh <139365289+Niansuh@users.noreply.github.com>
2024-01-08 06:14:44 -0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -538,7 +538,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>ChatGPT | Niansuh</div>
             <div className={styles["sub-title"]}>
               github.com/Niansuh/ChatGPT-Next-Web
             </div>
